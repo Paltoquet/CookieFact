fb912be0eddd18a6b4af466dde924c22c4975d5f
Alexandre CAZALA <alexandre.cazala@gmail.com>
2016-01-04 11:03:22 +0100

    fifo de commande

--- src/main/java/cod/tcf/Boulangerie.java
+++ src/main/java/cod/tcf/Boulangerie.java
@@ -1,8 +1,12 @@
 package cod.tcf;
 
+import java.util.LinkedList;
+
+import cod.ShoppingCart;
+
 public class Boulangerie {
 	private String name;
-	
+	private LinkedList<ShoppingCart> fifo = new LinkedList<ShoppingCart>();
 	public Boulangerie(String name) {
 		this.name = name;
 	}
@@ -11,6 +15,7 @@ public class Boulangerie {
 	}
 	
 	public void addCommand(ShoppingCart sc) {
-		
+            fifo.add (sc);
+            System.out.println("Ajout commande à boulangerie");
 	}
 }
